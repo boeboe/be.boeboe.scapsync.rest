67cbd91841e8d138ba10c50cc47409d48f1fda34
Bart Van Bos <bartvanbos@gmail.com>
2012-12-01 19:47:10 +0100

    (same as before, but without unneeded import)

--- src/be/boeboe/scapsync/rest/ScapSyncUtils.java
+++ src/be/boeboe/scapsync/rest/ScapSyncUtils.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.net.URI;
-import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
